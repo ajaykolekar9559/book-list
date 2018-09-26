f621b2257303dacec3464ab1c851c464dbc1232a
ajaykolekar9559 <ajaykolekar9559@gmail.com>
2018-09-26 18:30:57 +0530

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function(){
+    return[
+        {title : 'Shubham wade - The Wadhiv Karyakarta',pages :101},
+        {title : 'Tushar Dattu (Part 1) - The Game Changer',pages :34},
+        {title : 'DHiraj Jadhav - Best Deployer',pages :26},
+        {title : 'Tushar Dattu (Part 2) - Video Call Expert',pages :12},
+        {title : 'Ajay Kolekar : Chote Majak Hai kya',pages :1}
+     ]
+}
